68f349d686c217257adf6697879e1a410a2b3ce8
phamminhtu21 <148533247+phamminhtu21@users.noreply.github.com>
2024-05-03 23:07:28 +0700

    nop lab7

---
+++ bài th lab7/bai1.py
@@ -0,0 +1,5 @@
+pham_tu = {}
+n=int(input("Nhập n: "))
+for x in range(1,n+1):
+    pham_tu[x]=x**3
+print(f"tu dien ma cac phan tu co key la x va value cua key la x3 co do dai {n} la: \n", pham_tu)
---
+++ bài th lab7/bai2.py
@@ -0,0 +1,40 @@
+kho={
+    "banana": 6,
+    "apple": 5,
+    "orange": 32,
+    "pear": 15
+}
+
+gia_tien={
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+hoa_don={}
+tong_tien=0
+
+for mat_hang in kho:
+    so_luong=kho[mat_hang]
+    don_gia=gia_tien[mat_hang]
+    thanh_tien=so_luong*don_gia
+    hoa_don[mat_hang]={
+        "so_luong":so_luong,
+        "don_gia":don_gia,
+        "thanh_tien":thanh_tien
+    }
+    tong_tien+=thanh_tien
+
+print("hoa don mua hang :")
+for mat_hang, thong_tin in hoa_don.items():
+    print(f"mat hang:{mat_hang}")
+    print(f"So luong:{thong_tin['so_luong']}")
+    print(f"don gia:{thong_tin['don_gia']}dong")
+    print(f"So tien:{thong_tin['thanh_tien']}dong\n")
+
+print(f"tong so tien cua hoa don : {tong_tien}dong")
+
+print("\n so luong mat hang con trong kho:")
+for mat_hang,so_luong in kho.items():
+    print(f"{mat_hang}:{so_luong}")
